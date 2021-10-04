5ad2ed5b0aaf260720d594f1d095ac0ae80eb6c9
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-04 16:10:25 +0200

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import React, { FormEvent, useEffect, useState } from 'react';
 import './App.css';
-import { queries } from '@testing-library/dom';
+//import { StringifyOptions } from 'node:querystring';
+//import { queryAllByAltText } from '@testing-library/dom';
 import { IRecipe } from './IRecipe';
+import Recipe from './RecipeComponent';
+import { queries } from '@testing-library/dom';
 import RecipeComponent from './RecipeComponent';
 
 function App() {
   const [recipesFound, setRecipesFound] = useState<IRecipe[]>([]);
-  const [recipesSearch, setRecipesSearch] = useState ('');
+  const [recipeSearch, setRecipeSearch] = useState('');
 
-  const searchForRecipes= async(query: string): Promise<IRecipe[]> => {
+  const searchForRecipes = async (query: String): Promise<IRecipe[]> => {
     const result = await fetch(`http://localhost:3001/?search=${query}`)
-      return (await result.json()). results;
-  };
-
-  const search = (event:FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const input = form.querySelector('#searchText') as HTMLInputElement;
-    setRecipesSearch(input.value)
+    return (await result.json()).results;
   };
 
   useEffect(() => {
     (async () => {
-     
-      const query  =  encodeURIComponent(recipesSearch);
-      if (query) {
-        const response = await searchForRecipes(query);
-        setRecipesFound(response)
-      }
-      
+      const query = encodeURIComponent(recipeSearch);
+      const response = await searchForRecipes(query);
+      setRecipesFound(response);
     })();
-  },[recipesSearch]);
+  }, [recipeSearch]);
+
+  const search = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('#searchText') as HTMLInputElement;
+    setRecipeSearch(input.value);
+    input.value = '';
+  };
 
   return (
     <div className="App">
@@ -39,17 +39,13 @@ function App() {
         <input id="searchText" type="text" />
         <button>Search</button>
       </form>
-      {recipesSearch && <p>Results for {recipesSearch}... </p>}
-
-      <div className="recipe-container">
-      {recipesSearch && <p>Results for {recipesSearch}...</p>}
+      {recipeSearch && <p>Results for {recipeSearch}...</p>}
       <div className="recipes-container">
         {recipesFound.length &&
           recipesFound.map(recipe =>
             (<RecipeComponent key={recipe.href} recipe={recipe}></RecipeComponent>))
         }
       </div>
-    </div>
     </div>
   );
 }
